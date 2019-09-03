5aaee46ba55f121b3ba72a3894b873ae1eb8451d
Sam Flavin <flavi022@d.umn.edu>
2019-09-03 12:04:59 -0500

    working on addNewFamily saga

--- src/redux/sagas/familySaga.js
+++ src/redux/sagas/familySaga.js
@@ -1,8 +1,31 @@
 import { put, takeEvery } from 'redux-saga/effects';
-import axios from 'axios';
+import Axios from 'axios';
 
-function* familySaga () {
+
+//Post new user's family info to the DB
+function* addNewFamily(action) {
+    try {
+
+        console.log('in addNewFamily action.payload:', action.payload);
+
+        //This post should post new family data to DB referenceing new user id and payload will have family data
+        const response = yield Axios.post(`/family/${action.payload.id}`, action.payload);
+        console.log('addNewFamily back from server response:', response.data);
+        yield put({ type: 'SET_NEW_FAMILY', payload: response.data })
+
+    } catch (error) {
+
+        console.log('Error posting new family to DB:', error);
+       
+    }
+}
+
+function* familySaga() {
+
+    yield takeEvery('ADD_NEW_FAMILY', addNewFamily)
 
 }
+
+
 
 export default familySaga;
