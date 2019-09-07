843e2bf151e2be41c5200b386223c4c1df75a691
Sam Flavin <flavi022@d.umn.edu>
2019-09-07 18:01:05 -0500

    added modal to claim events

--- src/components/GroupView/GroupView.js
+++ src/components/GroupView/GroupView.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card, Image, Icon, Button, Feed } from 'semantic-ui-react';
+import { Card, Image, Icon, Button, Feed, Modal, Form } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+
 
 class GroupView extends Component {
 
@@ -23,8 +24,8 @@ class GroupView extends Component {
     render() {
     
         return (
-            <div>
-                <h1>
+            <div align="center">
+                <h1 align="center">
                    Welcome to the {this.props.reduxStore.userGroups && this.props.reduxStore.userGroups.length > 0 ?
                      this.props.reduxStore.userGroups[0].group_name : <p></p>} group!
                 </h1>
@@ -43,39 +44,53 @@ class GroupView extends Component {
                        
                         return (
                             <>
+                            <Card>
+                                    <Card.Content>
+
                             <Feed>
                         <Feed.Event>
                             <Feed.Label>
                             </Feed.Label>
                             <Feed.Content>
-                                <Feed.Label>
-                                 <img src={item.requester_image? item.requester_image : <>No</>} alt="lol" />
-                                </Feed.Label>
-                                 {item.requester_name} family needs a sitter on {item.event_date} from {item.event_time_start} - {item.event_time_end}. &nbsp;
-                                 <></><Button basic color='blue'>
-                                    CLAIM
-                                </Button>
+                                 The {item.requester_name} family needs a sitter on {item.event_date} from {item.event_time_start} - {item.event_time_end}. &nbsp;
+                                 <></>             
+                                                    <Modal trigger={<Button basic color='blue'>CLAIM</Button>}>
+                                                        <Modal.Header>{item.event_time_start} - {item.event_time_end} on {item.event_date}</Modal.Header>
+                                                        <Modal.Content image>
+                                                            <Image wrapped size='facebook' src={item.requester_image ? item.requester_image : <>No</>}/>
+                                                            
+                                                                <h5>Are you sure you want to deal with us?</h5>
+                                                            <Button basic color='green'>CONFIRM</Button><Button basic color='red'>CANCEL</Button>
+                                                        
+                                                        </Modal.Content>
+                                                    </Modal>
+                                                    
                             </Feed.Content>
                         </Feed.Event>
                     </Feed>
+                                        </Card.Content>
+
+                     </Card>
                             </>
                         )}
                         else {
                             return(
                             <>
+                            <Card>
                                 <Feed>
                                     <Feed.Event>
                                         <Feed.Label>
                                         </Feed.Label>
                                         <Feed.Content>
                                             <Feed.Label>
-                                                <img src={item.claimer_image} alt="lol" />
+                                                {/* <img src={item.claimer_image} alt="lol" /> */}
                                             </Feed.Label>
-                                                {item.claimer_name} has agreed to help the {item.requester_name} family on {item.event_date} from {item.event_time_start} - {item.event_time_end}. &nbsp;
+                                               The {item.claimer_name} family has agreed to help the {item.requester_name} family on {item.event_date} from {item.event_time_start} - {item.event_time_end}. &nbsp;
                     
                                         </Feed.Content>
                                     </Feed.Event>
                                 </Feed>
+                                </Card>
                             </>
                     )}
                     })
